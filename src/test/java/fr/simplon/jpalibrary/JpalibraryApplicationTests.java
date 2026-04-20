87976f5942b891a70abfe953b9b142edc5afdd72
OraneM99 <orane10@live.fr>
2026-04-20 14:43:46 +0200

    init project

---
+++ src/test/java/fr/simplon/jpalibrary/JpalibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.simplon.jpalibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpalibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
